cd255a004cc927c53cc65e091083c3f471d60465
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2025-12-25 22:53:46 +0530

    Initial Commit

---
+++ inspur-mgmnt-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  inspur-mgmnt-app
+//
+//  Created by Devasheesh Mishra on 25/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ inspur-mgmnt-app/inspur_mgmnt_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  inspur_mgmnt_appApp.swift
+//  inspur-mgmnt-app
+//
+//  Created by Devasheesh Mishra on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct inspur_mgmnt_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
